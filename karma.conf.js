833d6c06e3b3df519da59d9ed0af64ec0e8451d6
SmetDenis <denis@jbzoo.com>
2015-11-04 11:10:38 +0600

    fixed modules

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,13 @@ module.exports = function (config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_INFO,
 
         // Start these browsers, currently available:
         browsers: ['PhantomJS'],
 
         // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 30000,
+        captureTimeout: 10000,
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
--- tests/empty.js
+++ tests/empty.js
@@ -137,7 +137,9 @@
 
     describe("stripTags", function () {
         it("stripTags", function () {
-            expect(helper.stripTags('<p>Kevin</p> <br /><b>van</b> <i>Zonneveld</i>', '<i><b>')).toBe('Kevin <b>van</b> <i>Zonneveld</i>');
+            expect(helper.stripTags('<p>Kevin</p> <br /><b>van</b> <i>Zonneveld</i>', '<i><b>'))
+                .toBe('Kevin <b>van</b> <i>Zonneveld</i>');
+
             expect(helper.stripTags('<p>Kevin <img src="someimage.png" onmouseover="someFunction()">van <i>Zonneveld</i></p>', '<p>')).toBe('<p>Kevin van Zonneveld</p>');
             expect(helper.stripTags("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>", "<a>")).toBe("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>");
             expect(helper.stripTags('1 < 5 5 > 1')).toBe('1 < 5 5 > 1');
