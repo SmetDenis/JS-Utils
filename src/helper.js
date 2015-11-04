1d2ed84e1cdc139aef6513402c75a2f9219cf975
SmetDenis <denis@jbzoo.com>
2015-11-04 20:06:35 +0600

    register package

--- src/helper.js
+++ src/helper.js
@@ -61,8 +61,8 @@
          * @return {Boolean}
          */
         empty: function (mixedVar) {
-            var $this       = this, undef, key, i, len,
-                emptyValues = [undef, null, false, 0, '', '0'];
+            var $this       = this, undef, i, len,
+                emptyValues = [undefined, null, false, 0, '', '0'];
 
             for (i = 0, len = emptyValues.length; i < len; i++) {
                 if (mixedVar === emptyValues[i]) {
--- tests/empty.js
+++ tests/empty.js
@@ -140,12 +140,18 @@
             expect(helper.stripTags('<p>Kevin</p> <br /><b>van</b> <i>Zonneveld</i>', '<i><b>'))
                 .toBe('Kevin <b>van</b> <i>Zonneveld</i>');
 
-            expect(helper.stripTags('<p>Kevin <img src="someimage.png" onmouseover="someFunction()">van <i>Zonneveld</i></p>', '<p>')).toBe('<p>Kevin van Zonneveld</p>');
-            expect(helper.stripTags("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>", "<a>")).toBe("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>");
+            expect(helper.stripTags('<p>Kevin <img src="someimage.png" onmouseover="someFunction()">van <i>Zonneveld</i></p>', '<p>'))
+                .toBe('<p>Kevin van Zonneveld</p>');
+
+            expect(helper.stripTags("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>", "<a>"))
+                .toBe("<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>");
+
             expect(helper.stripTags('1 < 5 5 > 1')).toBe('1 < 5 5 > 1');
             expect(helper.stripTags('1 <br/> 1')).toBe('1  1');
             expect(helper.stripTags('1 <br/> 1', '<br>')).toBe('1 <br/> 1');
             expect(helper.stripTags('1 <br/> 1', '<br><br/>')).toBe('1 <br/> 1');
+            expect(helper.stripTags('1 <br> 1', '<br><br/>')).toBe('1 <br> 1');
+            expect(helper.stripTags(' 1 <br><br/> 1 ')).toBe(' 1  1 ');
         });
     });
 
